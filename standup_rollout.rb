e748b721411e319d3009e4a62df442c68f84a308
Daniel Quimper <d.quimper@samsung.com>
2021-08-27 12:12:17 -0400

    Uses ENV

--- standup_rollout.rb
+++ standup_rollout.rb
@@ -1,8 +1,11 @@
 require 'slack-notifier'
+require 'fileutils'
 
 class StandupRollout
   def initialize
-    webhook_url = "https://hooks.slack.com/services/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"
+    # webhook_url = "https://hooks.slack.com/services/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"
+    webhook_url = ENV["STANDUPROLLOUT_WEBHOOK_URL"].to_s
+    raise "STANDUPROLLOUT_WEBHOOK_URL is undefined" if webhook_url.empty?
     @notifier = Slack::Notifier.new webhook_url do
       defaults channel: "#slack-integration-tests",
                username: "StandupMaster"
@@ -12,16 +15,22 @@ class StandupRollout
   def notify(msg)
     # channel = "@U01PB4S6Z3J"
     # channel = "#secret-media-business"
-    channel = "#secret-gcore"
+    # channel = "#secret-gcore"
+    channel = ENV["STANDUPROLLOUT_CHANNEL"].to_s
+    raise "STANDUPROLLOUT_CHANNEL is undefined" if channel.empty?
+    at_handle = ENV["STANDUPROLLOUT_AT_HANDLE"].to_s
+    raise "STANDUPROLLOUT_AT_HANDLE is undefined" if at_handle.empty?
     # channel = "#slack-integration-tests"
     # full_msg = "<!subteam^S01NS71FSR5|devs-media-biz> #{msg}"
-    full_msg = "<!subteam^S0281KVFRFS|gcore> #{msg}"
+    # full_msg = "<!subteam^S0281KVFRFS|gcore> #{msg}"
+    full_msg = "#{at_handle} #{msg}"
     @notifier.ping full_msg, channel: channel
   end
 end
 
 standup_sm_list = ARGV
-scrum_masters_file = "#{__dir__}/scrum_masters.json"
+scrum_masters_file = "/tmp/standup_rollout/scrum_masters.json"
+FileUtils.mkdir_p(File.dirname(scrum_masters_file))
 
 begin
   selected_sm = JSON.parse(File.read(scrum_masters_file))
