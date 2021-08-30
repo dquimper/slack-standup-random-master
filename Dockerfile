5f743d35e8330bac2cf145816b68db0b7fde0c61
Daniel Quimper <d.quimper@samsung.com>
2021-08-30 13:49:26 -0400

    Adding env-file

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,4 @@ RUN bundle install
 
 COPY . .
 
-ENV STANDUPROLLOUT_WEBHOOK_URL=https://hooks.slack.com/services/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
-ENV STANDUPROLLOUT_CHANNEL='#secret-gcore'
-ENV STANDUPROLLOUT_AT_HANDLE='<!subteam^S0281KVFRFS|gcore>'
-
 CMD ["ruby", "standup_rollout.rb", "Martin", "Amrou", "Francois", "Ittsel", "Alex", "Guillaume", "Marion"]
--- docker/run.sh
+++ docker/run.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "-s" ]
 then
-  docker run -it --mount source=runtime,target=/tmp/standup_rollout adgear/standup_rollout:v1 bash
+  docker run -it --mount source=runtime,target=/tmp/standup_rollout --env-file .env adgear/standup_rollout:v1 bash
 else
-  docker run --mount source=runtime,target=/tmp/standup_rollout adgear/standup_rollout:v1
+  docker run --mount source=runtime,target=/tmp/standup_rollout --env-file .env adgear/standup_rollout:v1
 fi
